e5d9a607b8c215b5f865cfc73c76d85e919fb11d
dagmawib <dagmawibelayneh14@gmail.com>
2024-04-25 22:01:02 +0300

    Filters update

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ def welcome():
 @app.route('/home')
 def home():
     all_cafes = db.session.query(Cafe).all()
+    total_count =  len(all_cafes)
     filters = request.args.to_dict()
     
     if 'wifi' in filters:
@@ -55,9 +56,44 @@ def home():
         all_cafes = [cafe for cafe in all_cafes if cafe.has_sockets]
     if 'calls' in filters:
         all_cafes = [cafe for cafe in all_cafes if cafe.can_take_calls]
-
+    if 'longStay' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.can_stay_long]
+    if 'tables' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.seats]
+    if 'quite' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.is_quite]
+    if 'workVibe' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.work_vibe]
+    if 'group' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.work_with_group]
+    if 'coffee' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.has_coffee]
+    if 'food' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.has_food]
+    if 'veggie' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.has_veggie]
+    if 'alcohol' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.has_alcohol]
+    if 'cards' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.accept_card]
+    if 'lights' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.nice_lights]
+    if 'outdoor' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.has_outdoor]
+    if 'spacious' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.is_spaciou]
+    if 'restroom' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.has_toilet]
+    if 'accessible' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.is_accessible]
+    if 'ac' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.has_AC]
+    if 'pets' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.pets_allowed]
+    if 'parking' in filters:
+        all_cafes = [cafe for cafe in all_cafes if cafe.has_parking]
     remaining_count = len(all_cafes)
-    return render_template("home.html", all_cafes=all_cafes, remaining_count=remaining_count , page = 'home' )
+    return render_template("home.html", all_cafes=all_cafes, total_count = total_count,remaining_count=remaining_count , page = 'home' )
 
 
 @app.route("/cities")
